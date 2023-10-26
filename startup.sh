0ef5debba4b081dec5cc76f4a31489f16bc2b41c
Fabio Ewerton <fabioew89@gmail.com>
2023-10-25 22:56:36 -0400

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,11 +10,13 @@
 # Website   : fabio.eti.br
 # Social    : @FabioEw89
 
+# Version   : 1.0.19
+
 # +-------------------------------------------------------------------------+
 # |                               VARIABLES                                 |
 # +-------------------------------------------------------------------------+
 
-APT_UPDATE=(
+APT_UPDATE_LIST=(
     "update --fix-missing"
     "upgrade -y"
     "autoclean"
@@ -22,11 +24,11 @@ APT_UPDATE=(
     "install -f"
 )
 
-SCRIPTS=(
+SCRIPTS_LIST=(
     "https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh"
 )
 
-APT_INSTALL=(
+APT_INSTALL_LIST=(
     "git"
     "vim"
     "curl"
@@ -38,68 +40,58 @@ APT_INSTALL=(
 # |                               FUNCTIONS                                 |
 # +-------------------------------------------------------------------------+
 
-remove_locks(){
-    sudo rm /var/lib/dpkg/lock-frontend
-    sudo rm /var/cache/apt/archives/lock 
-}
+sudo rm -rf /var/lib/dpkg/lock-frontend && \
+sudo rm -rf /var/cache/apt/archives/lock # Remove locks?
 
 apt_update(){
-    for update in "${APT_UPDATE[@]}"; do
+    for update in "${APT_UPDATE_LIST[@]}"; do
         sudo apt $update >> /dev/null 2>&1
     done
 }
 
 apt_install(){
-    for pack in "${APT_INSTALL[@]}"; do
+    for pack in "${APT_INSTALL_LIST[@]}"; do
         if ! dpkg -l | grep -qw "^ii\s\+$pack"; then
             sudo apt install $pack -y >> /dev/null 2>&1
-            if [ $? -eq 0 ]; then
-                echo "[INFO] - O App $pack foi instalado com sucesso!"
-            else
-                echo "[ERROR] - Falha ao instalar o App $pack :("
-            fi       
-        else
-            echo "[INFO] - O App $pack já está instalado ;"
+            [ $? -eq 0 ] && echo "[INFO] - O App $pack foi instalado com sucesso!"
         fi
     done
 }
 
 install_scripts(){
-    for script in "${SCRIPTS[@]}"; do
+    for script in "${SCRIPTS_LIST[@]}"; do
         curl -s "$script" | sudo bash
     done    
 }
 
 install_oh_my_zsh() {
-    if ! dpkg -l | grep -qw "^ii\s\+zsh"; then
-        echo "[INFO] - Zsh não está instalado. Por favor, instale-o primeiro."
-        return 1
-    fi
+    local zsh_path="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"
+    local autosuggestions_path="$zsh_path/plugins/zsh-autosuggestions"
 
-    if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}" ]; then
-        echo "[INFO] - Instalando o Oh-my-zsh..."
-        sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" >> /dev/null 2>&1
-    else
-        echo "[INFO] - Oh-my-zsh já está instalado."
-    fi
+    # Verifique se o zsh está instalado
+    dpkg -l | grep -qw "^ii\s\+zsh" || \
+    { echo "[INFO] - Zsh não está instalado. Por favor, instale-o primeiro."; return 1; }
 
-    if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-        echo "[INFO] - Instalando o plugin zsh-autosuggestions..."
-        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions >> /dev/null 2>&1
-    else
-        echo "[INFO] - O plugin zsh-autosuggestions já está instalado."
-    fi
+    # Instale o Oh-my-zsh se não estiver presente
+    [ -d "$zsh_path" ] && echo "[INFO] - Oh-my-zsh já está instalado." || \
+    { echo "[INFO] - Instalando o Oh-my-zsh..."; \
+    sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" >> /dev/null 2>&1; }
+
+    # Instale o plugin zsh-autosuggestions se não estiver presente
+    [ -d "$autosuggestions_path" ] && echo "[INFO] - O plugin zsh-autosuggestions já está instalado." || \
+    { echo "[INFO] - Instalando o plugin zsh-autosuggestions..."; \
+    git clone https://github.com/zsh-users/zsh-autosuggestions $autosuggestions_path >> /dev/null 2>&1; }
 }
 
 # +-------------------------------------------------------------------------+
 # |                           CALL TO ACTION                                |
 # +-------------------------------------------------------------------------+
 
-remove_locks
 apt_update
 apt_install
 install_scripts
 install_oh_my_zsh
+apt_update
 
 # +-------------------------------------------------------------------------+
 # |                               THE END                                   |
