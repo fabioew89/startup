ee7da89f4c8abb911a4fbf72a0ce9b221146d0e0
fabio ewerton <fabioew89@gmail.com>
2024-01-31 15:01:57 -0400

    remote-pass.sh

---
+++ remote-pass.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo 'username?'
+read TAC_USERNAME
+
+echo 'password?'
+read TAC_PASS
+
+for host in {1..5}; do
+    sshpass -p $TAC_PASS ssh $TAC_USERNAME@100.127.0.$host 'sh run hostname ; show ip ospf neighbor | incl 100.127.0'
+echo " "
+done
