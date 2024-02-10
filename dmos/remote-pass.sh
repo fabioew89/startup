71b6ee976d8e2810a66ef59039c9ec7098a52dde
Fabio Ewerton <fabioew89@gmail.com>
2024-02-09 20:14:21 -0400

    created folder dmos

--- dmos/remote-pass.sh
+++ dmos/remote-pass.sh
@@ -3,7 +3,7 @@
 TAC_USERNAME="fabio.ewerton"
 
 for host in {1..5}; do
-    sshpass -f "password" ssh -o StrictHostKeyChecking=no "$TAC_USERNAME"@100.127.0.$host "sh run host" # < "config-dmos-alias.md"
+    sshpass -f password ssh -o StrictHostKeyChecking=no "$TAC_USERNAME"@100.127.0.$host "sh run host" # < "config-dmos-alias.md"
     echo " "
     echo "##################################################"
     # sleep 2
