ec22072fbfc9b727bc3d8d4698a9aa06128fa525
Fabio Ewerton <fabioew89@gmail.com>
2024-02-14 12:05:12 -0400

    add all

--- dmos/remote-auto-l2-alias.sh
+++ dmos/remote-auto-l2-alias.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-TAC_USERNAME="fabio.ewerton"
-
-for host in {1..5}; do
-    sshpass -f password ssh -o StrictHostKeyChecking=no "$TAC_USERNAME"@172.25.8.$host "sh run host" # < "config-dmos-alias.md"
+for host in {1..254}; do
+    sshpass -f password ssh -o StrictHostKeyChecking=no "fabio.ewerton"@172.25.8.$host  < "config/config-dmos-alias.md"
     echo " "
     echo "##################################################"
-    # sleep 2
+    echo " "
 done
