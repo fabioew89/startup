2904625d88270b577426e6c3aa2d64651d579deb
Fabio Ewerton <fabioew89@gmail.com>
2024-03-03 17:57:33 -0400

    modifie  remote-auto-ssh.sh

--- dmos/remote-auto-ssh.sh
+++ dmos/remote-auto-ssh.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
-username="fabio.ewerton"
+username=fabio.ewerton
 
-for host in {24..27}; do
-    echo ''
-    sshpass -f password ssh -o StrictHostKeyChecking=no "$username"@172.25.4.$host 'sh run hostname'
-    
-    echo ''
+# CORES
+# RED="\e[31m"
+# GREE="\e[32m"
+# RESET="\e[0"
+
+for host in 172.25.4.{24..27}; do
+
+    if ping -c 3 -q $host > /dev/null; then
+        echo
+        echo -e "\e[32m[INFO] - Equipamento "$host" esta pingando\n\e[0m"
+        sshpass -f password ssh -o StrictHostKeyChecking=no \
+        $username@$host "config ; sh hostname ; do show ip interface brief | include global"
+    else
+        echo -e "\e[31m\n[INFO] - Equipamento "$host" NÃO esta pingando\e[0m"
+    fi
+
+    echo
     for i in $( seq 15 ); do
         echo -n "#####"
     done
-    
-    echo ''
-    sleep 2
+    echo
 done
 
 
