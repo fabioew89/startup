c2747321e2b564e3a8b505095d9797cc5dd78532
Fabio Ewerton <fabioew89@gmail.com>
2024-03-05 11:42:21 -0400

    modified:   dmos/remote-auto-logout.sh

--- dmos/remote-auto-logout.sh
+++ dmos/remote-auto-logout.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
 USERNAME="fabio.ewerton"
-HOST="100.127.0.3"
+HOST="172.24.128.132"
 
 for host in {1..3}; do
-    sshpass -f password ssh -o StrictHostKeyChecking=no "$USERNAME"@"$HOST" \
-    "logout user "$USERNAME" ; who" # "config/config-dmos-logout.md"
-    echo ""
-    echo "##################################################"
-    echo ""
-    sleep 2
+    if ping -c 3 -q $HOST > /dev/null; then
+
+        echo -e "\n\e[32m[INFO] - Equipamento $HOST esta Online!\e[0m"
+        
+        sshpass -f password ssh -o StrictHostKeyChecking=no $USERNAME@$HOST \
+        "logout user $USERNAME"
+
+    else 
+        echo -e "\e[31m[INFO] - Equipamento $HOST NÃO esta Online!\e[0m"
+    fi
+
+
+    echo
+    for i in $( seq 10 ); do
+        echo -n "DATACOM"
+    done
+    echo
 done
