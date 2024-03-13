0fdb7bd9cd05eaaff2b4bba96d6d55e5885e494f
Fabio Ewerton <fabioew89@gmail.com>
2024-03-13 10:28:45 -0400

    modified:   dmos/remote-auto-loopback.sh

--- dmos/remote-auto-loopback.sh
+++ dmos/remote-auto-loopback.sh
@@ -6,7 +6,7 @@ for host in 100.127.0.{1..90}; do
         echo -e "\e[32m\n[INFO] - Equipamento "$host" esta pingando\n\e[0m"
         
         output=$(sshpass -f password ssh -o StrictHostKeyChecking=no 'fabio.ewerton'@$host \
-        "sh run hostname ; show running-config interface loopback 0 | select ipv4 address")
+        "sh run hostname ; show running-config interface loopback 0")
 
         output_host=$(echo "$output" | grep -i 'hostname' | cut -d ' ' -f 2)
         output_loop=$(echo "$output" | grep -i '100.127.0' | cut -d ' ' -f 4)
@@ -24,4 +24,7 @@ for host in 100.127.0.{1..90}; do
         echo -n "#####"
     done
     echo
+
+    break
+    
 done
