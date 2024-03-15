5f2605692d8b859e60ade68072e127c6abfbe60d
Fabio Ewerton <fabioew89@gmail.com>
2024-03-15 19:03:09 -0400

    modified:   dmos/remote-auto-tech-suport.sh

--- dmos/remote-auto-tech-suport.sh
+++ dmos/remote-auto-tech-suport.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 
 username='fabio.ewerton'
-tech_suporte_dir="$HOME/$(date +%Y-%m-%d)/tech-support/"
+hostname='100.127.0.27'
+date="$(date +%Y-%m-%d)"
+tech_support_dir="$HOME/$date/tech-support/"
+output_hostname="$( sshpass -f password ssh -o StrictHostKeyChecking=no $username@$hostname \
+                    "sh run hostname" | grep -i 'hostname' | cut -d ' ' -f 2)"
 
-for host in 100.127.0.{27..27} ; do
+output=$(sshpass -f password ssh -o StrictHostKeyChecking=no $username@$hostname \
+    "show running-config aaa | nomore | save $date-$output_hostname ; 
+    file delete $date-$output_hostname")
 
-    if ping -c 3 -q $host > /dev/null; then
-        echo -e "\e[32m\n[INFO] - Tech Support Extraido com sucesso do Equipamento "$host"\e[0m"
-        
-        output=$(sshpass -f password ssh -o StrictHostKeyChecking=no "$username"@"$host" \
-        "sh run hostname ; show tech-support")
+echo "$output"
 
-        output_hostname=$(echo "$output" | grep -i 'hostname' | cut -d ' ' -f 2)
+# [[ -d "$tech_support_dir" ]] || mkdir -p "$tech_support_dir"
 
-        [[ -d "$tech_suporte_dir" ]] || mkdir -p "$tech_suporte_dir"
+# if ping -c 3 -q $hostname > /dev/null; then
+#     echo -e "\e[32m\n[INFO] - Extraindo informações do host "$hostname"\e[0m"
 
-        echo "$output" > "$tech_suporte_dir/$output_hostname.txt"
+#     echo "$output" > "$tech_support_dir/$output_hostname.txt"
 
-    else
-        echo -e "\e[31m\n[INFO] - Equipamento "$host" NÃO esta pingando\e[0m"
-    fi
+# else
+#     echo -e "\e[31m\n[INFO] - Equipamento "$hostname" NÃO esta pingando\e[0m"
+# fi
 
-    echo
-    for i in $( seq 15 ); do
-        echo -n "#####"
-    done
-    echo
+echo
+for i in $( seq 15 ); do
+    echo -n "#####"
 done
+echo
