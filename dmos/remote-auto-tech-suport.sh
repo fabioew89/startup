893a0c8352ba746ff5cb4001861d4a30734299a5
Fabio Ewerton <fabioew89@gmail.com>
2024-03-17 19:05:24 -0400

    modified:   dmos/remote-auto-tech-suport.sh

--- dmos/remote-auto-tech-suport.sh
+++ dmos/remote-auto-tech-suport.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/env bash
 
-username='fabio.ewerton'
-hostname='100.127.0.27'
-date="$(date +%Y-%m-%d)"
-tech_support_dir="$HOME/$date/tech-support/"
-output_hostname="$( sshpass -f password ssh -o StrictHostKeyChecking=no $username@$hostname \
-                    "sh run hostname" | grep -i 'hostname' | cut -d ' ' -f 2)"
+USERNAME="fabio.ewerton"
+DATE="$(date +%F | sed s/-//g)"
+COMMAND="sh run mpls"
 
-output=$(sshpass -f password ssh -o StrictHostKeyChecking=no $username@$hostname \
-    "show running-config aaa | nomore | save $date-$output_hostname ; 
-    file delete $date-$output_hostname")
+for hostname in 100.127.0.{10..20}; do
 
-echo "$output"
+    output_hostname="$( sshpass -f password ssh -o StrictHostKeyChecking=no $USERNAME@$hostname \
+            "sh run hostname" | grep -i 'hostname' | cut -d ' ' -f 2)"
+    
+    output_hostname_file=$DATE-$output_hostname
 
-# [[ -d "$tech_support_dir" ]] || mkdir -p "$tech_support_dir"
+    if ping -c 3 -q $hostname > /dev/null; then
 
-# if ping -c 3 -q $hostname > /dev/null; then
-#     echo -e "\e[32m\n[INFO] - Extraindo informações do host "$hostname"\e[0m"
+        echo -e "\e[32m\n[INFO] - Extraindo informações do host "$hostname" - "$output_hostname"\e[0m\n"
 
-#     echo "$output" > "$tech_support_dir/$output_hostname.txt"
+        output=$(sshpass -f password ssh -o StrictHostKeyChecking=no $USERNAME@$hostname \
+            "$COMMAND | nomore | save "$output_hostname_file" ; 
+            copy file "$output_hostname_file" tftp://143.137.92.139/V8C4rxp9cM-incoming/tech-support/ ;
+            file delete "$output_hostname_file"")
 
-# else
-#     echo -e "\e[31m\n[INFO] - Equipamento "$hostname" NÃO esta pingando\e[0m"
-# fi
+        echo  "$output_hostname"
+        echo  "$output"
 
-echo
-for i in $( seq 15 ); do
-    echo -n "#####"
+    else
+        echo -e "\e[31m\n[INFO] - Equipamento "$hostname" - "$output_hostname" NÃO esta pingando\e[0m"
+    fi
+
+    echo
+    for _ in $( seq 10 ); do
+        echo -n "DATACOM "
+    done
+    echo
+
 done
-echo
