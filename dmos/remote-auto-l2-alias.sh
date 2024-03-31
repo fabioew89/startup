df8d9433523747ef4b7fdc3d9601a67b63611c8d
Fabio Ewerton <fabioew89@gmail.com>
2024-03-31 11:56:51 -0400

    rename all files - name

--- dmos/remote-auto-l2-alias.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for host in {1..254}; do
-    sshpass -f password ssh -o StrictHostKeyChecking=no "fabio.ewerton"@172.25.8.$host  < "config/config-dmos-alias.md"
-    echo " "
-    echo "##################################################"
-    echo " "
-done
