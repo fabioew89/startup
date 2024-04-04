c49ff447e3cfdb8f367b3c2eeaebb3b3eb554a78
Fabio Ewerton <fabioew89@gmail.com>
2024-04-04 18:44:20 -0400

    modified:   dmos/remote-upgrade.sh

--- dmos/remote-upgrade.sh
+++ dmos/remote-upgrade.sh
@@ -4,10 +4,10 @@ USERNAME="fabio.ewerton"
 COMMAND="show running-config hostname ;\
          show firmware"
 
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-ENDCOLOR="\e[0m"
+RED="\e[31;1;4m"
+GREEN="\e[32;1;4m"
+YELLOW="\e[33;1;4m"
+RESET="\e[0m"
 
 ssh_output(){
     ssh_output="$(sshpass -f password ssh -o StrictHostKeyChecking=no "$USERNAME"@"$ip_host" "$COMMAND")"
@@ -17,26 +17,23 @@ ssh_output(){
 }
 
 firmware_check_version(){
-    # get_device_firmware_version_int="$(echo "$get_device_firmware_version" | awk '{print int($0)}')"
-
-    if [ "$get_device_firmware_version" -ge 9 ]; then
-        echo -e "${GREEN}\natualizado 😍${ENDCOLOR}"
-    elif [ "$get_device_firmware_version" -le 8 ]; then
-        echo -e "${YELLOW}\nPrecisa de atualização 😒${ENDCOLOR}"
-    else
-        echo -e "${RED}\nPrecisa de atualização urgente 🥵${ENDCOLOR}"
-    fi
+    [ $get_device_firmware_version -ge 8 ] && \
+    echo -e "${GREEN}\nUp to date! 😁${RESET}" || \
+    echo -e "${YELLOW}\nNeeds updating! 😭${RESET}"
 }
 
 
 for ip_host in 100.127.0.{1..100}; do
     if ping -c 3 -q -W 3 "$ip_host" > /dev/null; then
         ssh_output
-        echo -e "${GREEN}\n[INFO] - Geting information about "$get_device_hostname" - "$ip_host"${ENDCOLOR}\n\
+        
+        echo -e "${GREEN}\n[INFO] - Geting information about "$get_device_hostname" - "$ip_host"${RESET}\n\
         \n$get_device_hostname\n$get_device_firmware"
+        
         firmware_check_version
+
     else
-        echo -e "${YELLOW}\n[INFO] - Sorry, better luck next time "$ip_host"${ENDCOLOR}"
+        echo -e "${RED}\n[INFO] - Sorry, better luck next time "$ip_host"${RESET}"
     fi
 
     # JUST SEPARATOR
