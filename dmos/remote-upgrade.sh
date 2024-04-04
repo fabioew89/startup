5dcc4c800c27d18c905b988685c2a76f1d0c1de7
Fabio Ewerton <fabioew89@gmail.com>
2024-04-04 17:15:39 -0400

    modified:   dmos/remote-upgrade.sh

--- dmos/remote-upgrade.sh
+++ dmos/remote-upgrade.sh
@@ -1,29 +1,43 @@
 #!/usr/bin/env bash
 
-username="fabio.ewerton"
-command="show running-config hostname ;\
+USERNAME="fabio.ewerton"
+COMMAND="show running-config hostname ;\
          show firmware"
 
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+ENDCOLOR="\e[0m"
+
 ssh_output(){
-    ssh_output=$(sshpass -f password ssh -o StrictHostKeyChecking=no "$username"@"$ip_host" "$command")
-    get_device_hostname=$(echo "$ssh_output" | grep -i hostname | cut -d ' ' -f 2)
-    get_device_firmware=$(echo "$ssh_output" | tail -2)
+    ssh_output="$(sshpass -f password ssh -o StrictHostKeyChecking=no "$USERNAME"@"$ip_host" "$COMMAND")"
+    get_device_hostname="$(echo "$ssh_output" | grep -i hostname | cut -d ' ' -f 2)"
+    get_device_firmware="$(echo "$ssh_output" | tail -2)"
+    get_device_firmware_version="$(echo "$ssh_output" | grep Active | cut -d "." -f 1)"
 }
 
-for ip_host in 100.127.0.{60..79}; do
-    if ping -c 3 -q -W 3 "$ip_host" > /dev/null; then
-        echo -e "\e[32m\n[INFO] - Geting information about "$get_device_hostname" - "$ip_host"\e[0m\n"
-        
-        ssh_output
-        
-        echo "$get_device_hostname"
-        echo "$get_device_firmware"
+firmware_check_version(){
+    # get_device_firmware_version_int="$(echo "$get_device_firmware_version" | awk '{print int($0)}')"
 
+    if [ "$get_device_firmware_version" -ge 9 ]; then
+        echo -e "${GREEN}\natualizado 😍${ENDCOLOR}"
+    elif [ "$get_device_firmware_version" -le 8 ]; then
+        echo -e "${YELLOW}\nPrecisa de atualização 😒${ENDCOLOR}"
     else
-        echo -e "\e[31m\n[INFO] - Sorry, better luck next time "$ip_host"\e[0m"
+        echo -e "${RED}\nPrecisa de atualização urgente 🥵${ENDCOLOR}"
     fi
+}
 
-    break
+
+for ip_host in 100.127.0.{1..100}; do
+    if ping -c 3 -q -W 3 "$ip_host" > /dev/null; then
+        ssh_output
+        echo -e "${GREEN}\n[INFO] - Geting information about "$get_device_hostname" - "$ip_host"${ENDCOLOR}\n\
+        \n$get_device_hostname\n$get_device_firmware"
+        firmware_check_version
+    else
+        echo -e "${YELLOW}\n[INFO] - Sorry, better luck next time "$ip_host"${ENDCOLOR}"
+    fi
 
     # JUST SEPARATOR
     echo
