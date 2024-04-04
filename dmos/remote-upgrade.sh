ce66f00d42773195a77955b1487fb00a07fb1834
Fabio Ewerton <fabioew89@gmail.com>
2024-04-04 11:52:50 -0400

    remote-upgrade.sh

---
+++ dmos/remote-upgrade.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+username="fabio.ewerton"
+command="show running-config hostname ;\
+         show firmware"
+
+for ip_host in 100.127.0.{60..79}; do
+    if ping -c 3 -q -W 3 "$ip_host" > /dev/null; then
+        echo -e "\e[32m\n[INFO] - Geting information about "$get_device_hostname" - "$ip_host"\e[0m\n"
+        
+        ssh_output=$(sshpass -f password ssh -o StrictHostKeyChecking=no "$username"@"$ip_host" "$command")
+        get_device_hostname=$(echo "$ssh_output" | grep -i hostname | cut -d ' ' -f 2)
+        get_device_firmware=$(echo "$ssh_output" | tail -2)
+        
+        echo "$get_device_hostname"
+        echo "$get_device_firmware"
+
+    else
+        echo -e "\e[31m\n[INFO] - Sorry, better luck next time "$ip_host"\e[0m"
+    fi
+
+    # JUST SEPARATOR
+    echo
+    for _ in $( seq 15 ); do
+        echo -n "##### "
+    done
+    echo   
+done
