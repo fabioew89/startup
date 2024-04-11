5b9e51b18fc29c2cbab9f039cdb7663df98b94aa
Fabio Ewerton <fabioew89@gmail.com>
2024-04-11 19:34:52 -0400

    dmos/remote-l2 && dmos/remote-l3

--- dmos/remote-l3-alias.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for host in {1..80}; do
-    sshpass -f password ssh -o StrictHostKeyChecking=no "fabio.ewerton"@100.127.0.$host  < "config/config-dmos-alias.md"
-    echo " "
-    echo "##################################################"
-    echo " "
-done
