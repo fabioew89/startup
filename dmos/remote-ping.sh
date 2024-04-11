89be410b071074968ccd21c1a0f05d331e87133a
Fabio Ewerton <fabioew89@gmail.com>
2024-04-11 19:34:29 -0400

    modified:   dmos/remote-ping.sh

--- dmos/remote-ping.sh
+++ dmos/remote-ping.sh
@@ -1,12 +1,46 @@
 #!/usr/bin/env bash
 
-for x in {1..100}; do
-  if ping -c 3 -W 1 "172.25.8.$x" > ; then
-    echo 'pingando'
-    else
-      echo 'não esta pingando'
-  fi
-done
+USERNAME='demo-user'
+COMMAND="show running-config hostname ; \
+         show firmware ; \
+         show ip int brief ; \
+         sh run int l3"
 
-#caixinha
-# cat macs.md | sed s/-/:/g | tr '[:upper:]' '[:lower:]'
+RED="\e[31;1m"
+GREEN="\e[32;1m"
+YELLOW="\e[33;1m"
+RESET="\e[0m"
+
+list_demo_device=(
+  'dm4170-demo.datacom.com.br'
+  'dm4370-demo.datacom.com.br'
+  'dm4050-demo.datacom.com.br'
+  'dm4610-demo.datacom.com.br'
+  'dm4610-demo-2.datacom.com.br'
+  'dm4610-demo-3.datacom.com.br'
+)
+
+ssh_output_device(){
+  ssh_output_device="$(sshpass -f password ssh -o StrictHostKeyChecking=no \
+  $USERNAME@$device_ip "$COMMAND")"
+}
+
+for device_ip in ${list_demo_device[@]}; do
+  ssh_output_device
+
+  get_device_hostname="$(echo "$ssh_output_device" | awk 'NR==1 { print $2 }')"
+  get_device_ip_int_brief="$(echo "$ssh_output_device" | awk '/Type Codes/,0')"
+  get_info_device="$(echo "$ssh_output_device")"
+
+  # echo -e "\n${GREEN}$get_device_hostname\n$get_device_ip_int_brief${RESET}"
+
+  echo -e "${RED}$get_device_hostname\n${YELLOW}$ssh_output_device${RESET}"
+
+  echo
+  for i in $(seq 5); do
+    echo -n '##### '
+  done
+  echo
+
+  break
+done
