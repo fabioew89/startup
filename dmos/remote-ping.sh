79ea54e857a621b3a914e6b58b2b15238d9f8eb7
Fabio Ewerton <fabioew89@gmail.com>
2024-04-11 20:22:58 -0400

    modified:   dmos/remote-ping.sh

--- dmos/remote-ping.sh
+++ dmos/remote-ping.sh
@@ -7,7 +7,7 @@ COMMAND="show running-config hostname ; \
          sh run int l3"
 
 RED="\e[31;1m"
-GREEN="\e[32;1m"
+# GREEN="\e[32;1m"
 YELLOW="\e[33;1m"
 RESET="\e[0m"
 
@@ -22,25 +22,35 @@ list_demo_device=(
 
 ssh_output_device(){
   ssh_output_device="$(sshpass -f password ssh -o StrictHostKeyChecking=no \
-  $USERNAME@$device_ip "$COMMAND")"
-}
+  $USERNAME@"$device_ip" "$COMMAND")"
 
-for device_ip in ${list_demo_device[@]}; do
-  ssh_output_device
-
+  # VARIABLE
   get_device_hostname="$(echo "$ssh_output_device" | awk 'NR==1 { print $2 }')"
   get_device_ip_int_brief="$(echo "$ssh_output_device" | awk '/Type Codes/,0')"
-  get_info_device="$(echo "$ssh_output_device")"
+  # get_info_device="$(echo "$ssh_output_device")"
 
-  # echo -e "\n${GREEN}$get_device_hostname\n$get_device_ip_int_brief${RESET}"
 
-  echo -e "${RED}$get_device_hostname\n${YELLOW}$ssh_output_device${RESET}"
+}
+
+for device_ip in "${list_demo_device[@]}"; do
+  if ping -c 3 -q -W 3 "$device_ip" > /dev/null 2>&1; then 
+
+    ssh_output_device
+
+    echo -e "\n${GREEN}${get_device_hostname}\n${get_device_ip_int_brief}${RESET}"
+    echo -e "${RED}$get_device_hostname\n${YELLOW}$ssh_output_device${RESET}"
+
+  else
+    echo -e "\n${RED}Equipamento offline${RESET}"
+
+  fi
 
   echo
+  # shellcheck disable=SC2034
   for i in $(seq 5); do
     echo -n '##### '
   done
-  echo
+  echo  
 
   break
 done
