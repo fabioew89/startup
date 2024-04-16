7abc61bcd59127f5398a732cca676bb696b63bb8
Fabio Ewerton <fabioew89@gmail.com>
2024-04-16 18:50:35 -0400

    modified:   dmos/remote-alias.sh

--- dmos/remote-alias.sh
+++ dmos/remote-alias.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-USERNAME='fabio.ewerton'
+USERNAME="fabio.ewerton"
 
 for host in {1..254}; do
     sshpass -f password ssh -o StrictHostKeyChecking=no \
-    "$USERNAME"@172.25.8.$host  < "config/config-dmos-alias.md"
+    "$USERNAME"@172.25.8."$host"  < "config/config-dmos-alias.md"
 
     # JUST SEPARATOR
     echo
-    for i in $(seq 15); do
+    for _ in $(seq 15); do
       echo -n "##### "
     done
     echo
