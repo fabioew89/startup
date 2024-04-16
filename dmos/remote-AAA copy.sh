56c043e46ddf8541c18ead91970dc9e50a23f142
Fabio Ewerton <fabioew89@gmail.com>
2024-04-16 18:50:20 -0400

    modified:   dmos/remote-AAA copy.sh

--- dmos/remote-AAA copy.sh
+++ dmos/remote-AAA copy.sh
@@ -3,9 +3,8 @@
 USERNAME="fabio.ewerton"
 COMMAND="show running-config hostname ;\
          show running-config aaa"
-# shellcheck disable=SC2016
+         
 aaa_pass_admin=$(cat password | awk 'NR == 2')
-# shellcheck disable=SC2016
 aaa_pass_tacacs=$(cat password | awk 'NR == 3')
 
 RED="\e[31;1m"
@@ -14,10 +13,19 @@ YELLOW="\e[33;1m"
 RESET="\e[0m"
 
 ssh_output(){
-    ssh_output="$(sshpass -f password ssh -o StrictHostKeyChecking=no "$USERNAME"@"$ip_host" "$COMMAND")"
+    ssh_output="$(sshpass -f password ssh -o StrictHostKeyChecking=no "$USERNAME"@"$ip_address" "$COMMAND")"
     get_device_hostname="$(echo "$ssh_output" | awk 'NR==1 { print $2 }')"
+    
     get_device_aaa_pass_admin=$(echo "$ssh_output" | grep -i password | awk '{ print $2 }')
     get_device_aaa_pass_tacacs=$(echo "$ssh_output" | grep -i shared | awk '{ print $2 }')
+    
+    get_device_aaa_users=$(echo "$ssh_output" | grep -i aaa | awk 'NR > 1 { print $3 }')
+    get_device_aaa_user_luan=$(echo "$ssh_output" | grep -i luan | awk '{ print $3 }')
+}
+
+ssh_config(){
+    sshpass -f password ssh -o StrictHostKeyChecking=no -t \
+    "$USERNAME"@"$ip_address" < "config/config-dmos-aaa.md"
 }
 
 # firmware_check_version(){
@@ -27,33 +35,34 @@ ssh_output(){
 # }
 
 
-for ip_host in 100.127.0.{1..100}; do
-    if ping -c 3 -q -W 3 "$ip_host" > /dev/null; then
+for ip_address in 100.127.0.{1..5}; do
+    if ping -c 3 -q -W 3 "$ip_address" > /dev/null 2>&1; then
        
         ssh_output
 
-        echo -e "\n${GREEN}[INFO] - Geting information about $get_device_hostname - $ip_host${RESET}"
-        # echo -e "\n$get_device_hostname\n$get_device_aaa_pass_admin\n$get_device_aaa_pass_tac"
-
+        echo -e "\n${GREEN}[INFO] - Geting information about \
+        $get_device_hostname - $ip_address${RESET}"
+        
         if [ "$get_device_aaa_pass_admin" == "$aaa_pass_admin" ] && \
-           [ "$get_device_aaa_pass_tacacs" == "$aaa_pass_tacacs" ]; then
+           [ "$get_device_aaa_pass_tacacs" == "$aaa_pass_tacacs" ] || \
+           [ "$get_device_aaa_user_luan" == "luan" ]; then
             
-            echo -e "${GREEN}Tacacs configura corretamente${RESET}"
-            echo -e "$get_device_aaa_pass_admin\n$get_device_aaa_pass_tacacs"
+            echo -e "\n${GREEN}$get_device_aaa_users${RESET}"
             
         else
 
-            echo -e "${RED}fora de padrão${RESET}"
-            echo -e "$get_device_aaa_pass_admin\n$get_device_aaa_pass_tacacs"
+            echo -e "\n${YELLOW}$get_device_aaa_users${RESET}"
+            ssh_config
+
         fi
         
     else
-        echo -e "${RED}\n[INFO] - Sorry, better luck next time $ip_host${RESET}"
+        echo -e "${RED}\n[INFO] - Sorry, better luck next time $ip_address${RESET}"
     fi
 
     # JUST SEPARATOR
     echo
-    for _ in $(seq 15); do
+    for _ in $(seq 15 ); do
         echo -n "##### "
     done
     echo
